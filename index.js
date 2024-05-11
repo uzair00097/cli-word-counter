0bfd5885279cb042d1d939a3e25d227838193482
Muhammad Uzair <160115627+uzair00096@users.noreply.github.com>
2024-05-11 22:11:47 +0500

    Adding a CLI based Word Counter Project

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "Input",
+        message: "Enter your sentence to count the word:"
+    }
+]);
+const words = answer.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
